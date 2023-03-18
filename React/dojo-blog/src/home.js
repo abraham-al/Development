bfb19b0b6e0f81428ffc57492c21d05c575195e3
abraham-al <abraham.alemseged.f@gmail.com>
2023-03-18 13:25:10 +0300

    React App

--- React/dojo-blog/src/home.js
+++ React/dojo-blog/src/home.js
@@ -1,22 +1,15 @@
 import { useState, useEffect } from 'react'
 import BlogList from './Bloglist';
-
+import useFetch from './useFetch';
 
 const Home = () => {
-    let name = 'ABCDef'
-    const [blogs, setBlogs] = useState([
-        { title: "My new Website", body: "lorem ipsum ...", author:"Abraham", id:1 },
-        { title: "Welcome Party", body: "lorem ipsum ...", author:"Abie", id:2 },
-        { title: "Web Dev", body: "lorem ipsum ...", author:"Melaku", id:3 }
-    ]);
-
-    const handleDelete = (id) => {
-
-    }
-
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title = "All Blogs"/>
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title = "All Blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Abraham")} title = "Abraham's Blogs"/> */}
         </div>
      );
